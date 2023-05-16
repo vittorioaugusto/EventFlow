1caa84fff350faff11e2bcc0bfc0c424cafca2c7
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-16 09:53:06 -0300

    update

--- principal.php
+++ principal.php
@@ -21,13 +21,17 @@ $dado = mysqli_fetch_row($query);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <script>
+        function consultarUsuario() {
+            window.location.href = "consultar.php";
+        }
+    </script>
 </head>
 <body>
-
     <h1>Menu principal <?php echo $dado[0]; ?>!</h1>
 
     <nav>
-    <button>Eventos</button><br>
+            <button onclick="consultarUsuario()">Consultar Usuário</button><br>
     </nav>
 </body>
 </html>
