f5d17a8168b6cb2384ce2d6b9ac43e4717e92e5b
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-19 09:35:55 -0300

    update

--- autenticar.php
+++ autenticar.php
@@ -7,12 +7,6 @@ $email = isset($_POST['email']) ? $_POST['email'] : '';
 $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
 $select = "SELECT * FROM login WHERE email = '$email' AND senha = '$senha'";
-
-/*
-POSTGRES
-$query = pg_exec($conexao, $select);login
-$dado = pg_fetch_row($query);
-*/
 $query = mysqli_query($conexao, $select);
 $dado = mysqli_fetch_row($query);
 
