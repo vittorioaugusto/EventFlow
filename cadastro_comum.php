20979601b43317433e72c88f5797e3b78e9e2f4e
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-19 11:39:28 -0300

    update

--- cadastro_comum.php
+++ cadastro_comum.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro Comum</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <form class="form_cadastro_comum" action="autenticar.php" method="POST">
--- tipo_de_usuario.php
+++ tipo_de_usuario.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tipo de Usuário</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+
     <div class="fundo_tipo_cadastro">
         
             <div class="logo_usuario">
