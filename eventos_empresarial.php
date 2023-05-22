8efeedf0dfd56f073c19400808073bad036be5fe
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-22 17:59:59 -0300

    update

--- eventos_empresarial.php
+++ eventos_empresarial.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela Principal | Usuário Empresarial</title>
+    <title>Eventos Empresarial</title>
     <link rel="stylesheet" href="assets/css/principal_empresarial.css">
 
 </head>
