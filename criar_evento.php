488007202306caa1fdc8ecd5298eccdaedec5f96
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-22 18:01:24 -0300

    update

--- criar_evento.php
+++ criar_evento.php
@@ -5,8 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Evento</title>
+    <link rel="stylesheet" href="assets/css/principal_empresarial.css">
 </head>
 <body>
+<div class="cabecalho">
+        <div class="logo_principal">
+            <img src="assets/imagens/logo_fundo_removido.png" alt="Logo EventFlow">
+        </div>
     
+        <nav class="botoes">
+            <a href="principal_empresarial.php"> <label>Inicio</label></a>
+            <a href="eventos_empresarial.php"> <label>Criar Evento</label></a>
+            <a href="perfil_empresarial.php"> <label>Perfil</label></a>       
+        </nav>
+        
 </body>
 </html>
