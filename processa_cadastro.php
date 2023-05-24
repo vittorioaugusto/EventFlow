9582c1382f6b4f242e56e8b70bd5cc6fa2eba287
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-24 15:16:43 -0300

    update

--- processa_cadastro.php
+++ processa_cadastro.php
@@ -1,7 +1,4 @@
-// processa_cadastro.php
-
-<?php /*
-
+<?php
 include('conexao.php');
 
 $nome = isset($_POST['nome']) ? $_POST['nome'] : '';
@@ -10,37 +7,12 @@ $telefone = isset($_POST['telefone']) ? $_POST['telefone'] : '';
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 $tipo_user = isset($_POST['tipo_user']) ? $_POST['tipo_user'] : '';
-
-$insert_usuario = "INSERT INTO usuario (nome, cpf_cnpj, tipo_user, telefone) VALUES ('$nome', '$cpf_cnpj', '$tipo_user', '$telefone')";
-$query_usuario = mysqli_query($conexao, $insert_usuario);
-
-$id_usuario = mysqli_insert_id($conexao);
-
-$insert_login = "INSERT INTO login (email, senha, idusuario) VALUES ('$email', '$senha', '$id_usuario')";
-$query_login = mysqli_query($conexao, $insert_login);
-
-if ($query_usuario && $query_login) {
-    header("location: login.php");
-} else {
-    echo "Erro ao cadastrar usuário. Por favor, tente novamente.";
-}
-*/
-
-
-
-include('conexao.php');
-
-$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
-$cpf_cnpj = isset($_POST['cpf_cnpj']) ? $_POST['cpf_cnpj'] : '';
-$telefone = isset($_POST['telefone']) ? $_POST['telefone'] : '';
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
-$tipo_user = isset($_POST['tipo_user']) ? $_POST['tipo_user'] : '';
+$empresa = isset($_POST['empresa']) ? $_POST['empresa'] : '';
 
 // Prepara a consulta para inserir o usuário
-$insert_usuario = "INSERT INTO usuario (nome, cpf_cnpj, tipo_user, telefone) VALUES (?, ?, ?, ?)";
+$insert_usuario = "INSERT INTO usuario (nome, cpf_cnpj, tipo_user, telefone, empresa) VALUES (?, ?, ?, ?, ?)";
 $stmt_usuario = mysqli_prepare($conexao, $insert_usuario);
-mysqli_stmt_bind_param($stmt_usuario, "ssis", $nome, $cpf_cnpj, $tipo_user, $telefone);
+mysqli_stmt_bind_param($stmt_usuario, "ssiss", $nome, $cpf_cnpj, $tipo_user, $telefone, $empresa);
 $query_usuario = mysqli_stmt_execute($stmt_usuario);
 
 // Obtém o ID do usuário inserido
@@ -54,9 +26,8 @@ $query_login = mysqli_stmt_execute($stmt_login);
 
 if ($query_usuario && $query_login) {
     header("location: login.php");
+    exit();
 } else {
     echo "Erro ao cadastrar usuário. Por favor, tente novamente.";
 }
-
-
 ?>
