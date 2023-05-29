8bf02bbc575def2b42be05d1ee6f6b71b5b06aae
vittorioaugusto <vi.aborges47@gmail.com>
2023-05-29 12:44:29 -0300

    update login

--- login.php
+++ login.php
@@ -1,37 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-</head>
-<body>
-    <center>
-        <form class="form_login" action="autenticar.php" method="POST">
-
-            <div class="logo_login">
-                <img src="assets/imagens/logo_fundo_removido.png" alt="Logo EventFlow">
-            </div>
-            <div class="login">
-                <input type="email" placeholder="Email" name="email" required><br>
-                <input type="password" placeholder="Senha" name="senha" required><br>
-            </div>
-            <div class="entrar_login">
-                <button type="submit" name="enviar" value="Entrar"><h1>Entrar</h1></button><br>
-            </div>
-            <div class="senha_e_conta">
-                
-            </div>
-            
-        </form>
-    </center>
-    
-    
-</body>
-</html> -->
-
 <?php
 session_start();
 ?>
