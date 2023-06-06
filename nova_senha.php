21a607ba768aaf738d20103cb16a90187513ca18
vittorioaugusto <vi.aborges47@gmail.com>
2023-06-06 18:03:45 -0300

    update

--- nova_senha.php
+++ nova_senha.php
@@ -1,22 +1,4 @@
 <?php
-
-session_start();
-
-include('conexao.php');
-
-$idusuario = $_SESSION['idusuario'];
-
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$senha = isset($_POST['nova_senha']) ? $_POST['nova_senha'] : '';
-
-$update = "UPDATE login SET senha = '$senha' WHERE idusuario = '$idusuario'";
-$query = mysqli_query($conexao, $update);
-
-header('Location: login.php');
-
-?>
-
-<?php
 include('conexao.php');
 
 $idusuario = $_SESSION['idusuario'];
