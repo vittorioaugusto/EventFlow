67b7af0c1c6368519ecde12e91de2224db5e59a9
vittorioaugusto <vi.aborges47@gmail.com>
2023-06-22 11:06:46 -0300

    update 37

--- processar_compra.php
+++ processar_compra.php
@@ -108,17 +108,29 @@
                         echo "<p>Preço Total: R$ $preco_total</p>";
 
                         // Exibir formulário de avaliação
-                        echo '<div class="avaliacao">';
-                        echo '<h2>Avaliar o Evento</h2>';
-                        echo '<form action="processar_avaliacao.php" method="post">';
-                        echo '<label for="nota">Nota:</label>';
-                        echo '<input type="radio" name="nota" value="5"> 5';
-                        echo '<input type="radio" name="nota" value="4"> 4';
-                        echo '<input type="radio" name="nota" value="3"> 3';
-                        echo '<input type="radio" name="nota" value="2"> 2';
-                        echo '<input type="radio" name="nota" value="1"> 1';
-                        echo '</form>';
-                        echo '</div>',"<br>";
+                        echo'<h2 id="nome_avaliar_evento">Avaliar o Evento</h2>';
+                        echo'<div class="rating">
+                        <input value="star-1" name="star-radio" id="star-1" type="radio">
+                        <label for="star-1">
+                          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                        </label>
+                        <input value="star-1" name="star-radio" id="star-2" type="radio">
+                        <label for="star-2">
+                          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                        </label>
+                        <input value="star-1" name="star-radio" id="star-3" type="radio">
+                        <label for="star-3">
+                          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                        </label>
+                        <input value="star-1" name="star-radio" id="star-4" type="radio">
+                        <label for="star-4">
+                          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                        </label>
+                        <input value="star-1" name="star-radio" id="star-5" type="radio">
+                        <label for="star-5">
+                          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                        </label>
+                      </div>';
 
                         echo "<a href='eventos.php'>Voltar para os Eventos</a>";
                     } else {
@@ -136,5 +148,5 @@
             </div>
         </div>
     </div>  
-</body>
+</body> 
 </html>
