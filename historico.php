9b997bce3c9b6cf9855c1014b76e06d0404ea32e
vittorioaugusto <vi.aborges47@gmail.com>
2023-06-27 09:57:00 -0300

    update 42

--- historico.php
+++ historico.php
@@ -8,9 +8,8 @@
 </head>
 <body>
     <div class="cabecalho_historico">
-
         <div class="logo_historico">
-        <a href="eventos.php"><img src="assets/imagens/logo_fundo_removido.png" alt="Logo EventFlow" title="Início" width="200"></a>
+            <a href="eventos.php"><img src="assets/imagens/logo_fundo_removido.png" alt="Logo EventFlow" title="Início" width="200"></a>
         </div>
 
         <div class="container_historico">
@@ -38,24 +37,48 @@
 
                 // Executa a query
                 $resultado = mysqli_query($conexao, $query);
-                echo'<div class="caixa_historio">';
+
                 // Verifique se a consulta retornou resultados
                 if (mysqli_num_rows($resultado) > 0) {
+                    echo'<div class="caixa_historio">';
+                    // Variável para verificar se ocorreu duplicação de ingressos
+                    $ingressosDuplicados = false;
+                    $codigosIngresso = array();
+
                     // Loop pelos resultados e exibição dos itens comprados
                     while ($row = mysqli_fetch_assoc($resultado)) {
                         echo "Item comprado: " . $row['nome_item'] . "<br>";
                         echo "Quantidade: " . $row['quantidade'] . "<br>";
-                        echo "Preço unitário: " . $row['preco_unitario'] . "<br>","<hr>"; 
+                        echo "Preço unitário: " . $row['preco_unitario'] . "<br>";
+
+                        // Verifique se há duplicação de ingressos
+                        if ($row['cod_ingressos'] !== null) {
+                            $codigos = explode(",", $row['cod_ingressos']);
+                            foreach ($codigos as $codigo) {
+                                if (in_array($codigo, $codigosIngresso)) {
+                                    $ingressosDuplicados = true;
+                                    break;
+                                } else {
+                                    $codigosIngresso[] = $codigo;
+                                }
+                            }
+                        }
+
+                        echo "<hr>";
                         echo "<br>";
                     }
-                    
+
+                    // Exibir mensagem de aviso caso ocorra duplicação de ingressos
+                    if ($ingressosDuplicados) {
+                        echo '<span class="aviso">Atenção: Ocorreu uma duplicação de códigos de ingressos. Isso não interfere na sua compra e no valor descontado. Cada ingresso é único e válido separadamente.</span><br>';
+                    }
                 } else {
-                    echo'<center>';
+                    echo '<center>';
                     echo "Nenhum item comprado encontrado.";
-                    
                 }
-                echo'<a href="eventos.php">Voltar para Eventos</a>';
-                echo'</center>';
+                echo '<center>';
+                echo '<a href="eventos.php">Voltar para Eventos</a>';
+                echo '</center>';
                 echo'</div>';
                 // Feche a conexão com o banco de dados
                 mysqli_close($conexao);
@@ -64,4 +87,4 @@
         </div>
     </div> 
 </body>
-</html> 
+</html>
